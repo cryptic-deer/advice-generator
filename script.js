5c26014fc92c45dfc8c777a18b6c25fa8a98fc1a
cryptic-deer <artist.tibi@gmail.com>
2022-06-29 16:47:43 +0200

    Finish functionality

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-const getAdvice = () => {
-	axios
+const getAdvice = async () => {
+	await axios
 		.get("https://api.adviceslip.com/advice")
 		.then(response => {
-			console.log(`${response.data.slip.id}\n${response.data.slip.advice}`);
+			createContent(response.data.slip.id, response.data.slip.advice);
 		})
 		.catch(err => {
-			console.error(err);
+			console.log(err);
+			createContent("error", "Something went wrong, try again later!");
 		});
 };
 
 getAdvice();
+
+const content = document.querySelector("#content");
+const diceBtn = document.querySelector("#diceBtn");
+
+const adviceId = document.createElement("h1");
+const adviceText = document.createElement("p");
+
+const createContent = (id, text) => {
+	adviceId.textContent = `advice #${id}`;
+	adviceText.textContent = `"${text}"`;
+
+	content.appendChild(adviceId);
+	content.appendChild(adviceText);
+};
+
+diceBtn.addEventListener("click", () => {
+	getAdvice();
+});
